ae14cc7a4a7244a31f1757f51452025dd1b9021a
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 12:26:37 +0530

    created getter and setter methods

--- src/main/java/com/niiit/jdp/product/Product.java
+++ src/main/java/com/niiit/jdp/product/Product.java
@@ -22,4 +22,36 @@ public class Product {
         this.productStatus = productStatus;
         ProductReview = productReview;
     }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public String getProductReview() {
+        return ProductReview;
+    }
+
+    public void setProductReview(String productReview) {
+        ProductReview = productReview;
+    }
 }
