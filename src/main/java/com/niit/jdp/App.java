a53e1e216a34fc193caed4510abff873d01f7b19
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 13:10:22 +0530

    calling all the products inside the main method

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -1,10 +1,23 @@
 package com.niit.jdp;
 
-/**
- * Hello world!
- */
+import com.niit.jdp.config.ProductConfig;
+import com.niit.jdp.domain.Product;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProductConfig.class);
+        //calling product1
+        Product product1 = context.getBean("product1", Product.class);
+        System.out.println("Product_1 = " + product1);
+        //calling product2
+        Product product2 = context.getBean("product2", Product.class);
+        System.out.println("Product_2 = " + product2);
+        //calling product3
+        Product product3 = context.getBean("product3", Product.class);
+        System.out.println("Product_3 = " + product3);
+
+
     }
 }
