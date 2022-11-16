abab12c9a34538ad6687060638154daad38b8b32
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 13:13:35 +0530

    final commit

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+        /* Creating a new instance of the `AnnotationConfigApplicationContext` class and passing the `ProductConfig` class
+         as a parameter.*/
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProductConfig.class);
-        //calling product1
+
+        // Calling the `product1` bean from the `ProductConfig` class.
         Product product1 = context.getBean("product1", Product.class);
         System.out.println("Product_1 = " + product1);
-        //calling product2
+
+        // Calling the `product2` bean from the `ProductConfig` class.
         Product product2 = context.getBean("product2", Product.class);
         System.out.println("Product_2 = " + product2);
-        //calling product3
+
+        // Calling the `product3` bean from the `ProductConfig` class.
         Product product3 = context.getBean("product3", Product.class);
         System.out.println("Product_3 = " + product3);
 
